925dcfaeefb7481870c6bfb604ee64d2688e3d0a
krypek <krypek@tuta.io>
2024-05-04 20:29:30 +0200

    Format the code

--- plugin.js
+++ plugin.js
@@ -1,19 +1,16 @@
-
-export default class ExtendableHeads extends Plugin  {
+export default class ExtendableHeads extends Plugin {
 	async prestart() {
-		
 		// Image loaded successfully
 		const oldImage = await this.loadImage("media/gui/severed-heads.png");
 
 		const {headIdx} = await this.getHeadIdx();
 		let imgs = [oldImage];
-		
-	
+
 		// assume each image is 24x24
 		for (let index = 0; index < headIdx.length; ++index) {
 			const head = headIdx[index];
 			try {
-				imgs.push((await this.loadImage(head.img)));
+				imgs.push(await this.loadImage(head.img));
 			} catch (e) {
 				headIdx.splice(index, 1);
 				--index;
@@ -25,21 +22,16 @@ export default class ExtendableHeads extends Plugin  {
 		const newImage = await this.mergeImages(imgs);
 		const dim = this.getTotalDim([newImage]);
 
-		
 		const startIndex = this.calcStartIndex(dim, headIdx);
 
-		
-
-		
-		
 		const customIdx = {};
 		let customNameMapping = {};
 
 		for (let index = 0; index < headIdx.length; ++index) {
 			const head = headIdx[index];
 			customIdx[head.id] = startIndex + index;
-			if(head.name) {
-				if(head.name in customNameMapping) {
+			if (head.name) {
+				if (head.name in customNameMapping) {
 					console.warn(`Warning: duplicate name entry "${head.name}" for custom head of ${head.id}`);
 				} else {
 					customNameMapping[head.name] = startIndex + index;
@@ -47,45 +39,43 @@ export default class ExtendableHeads extends Plugin  {
 			}
 		}
 		sc.PlayerConfig.inject({
-			onload: function(config) {
+			onload: function (config) {
 				if (!config.jsonTEMPLATES) {
 					const id = config.character;
 					if (id in customIdx) {
-						
 						config.headIdx = customIdx[id];
 					}
 				}
 				this.parent(config);
-			}
+			},
 		});
 
 		sc.SaveSlotParty.inject({
-			setParty: function({player}) {
+			setParty: function ({player}) {
 				try {
 					this.party[0] = sc.party.models[player.playerConfig].getHeadIdx();
 				} catch (e) {}
-				
+
 				return this.parent.apply(this, arguments);
-			}
+			},
 		});
 
-
 		sc.CombatUpperHud.inject({
-			init: function() {
+			init: function () {
 				this.parent();
 				const pvp = this.sub.pvp;
 				const old = pvp._renderHeads;
-				pvp._renderHeads = function(renderer, x, flip, idxArr) {
+				pvp._renderHeads = function (renderer, x, flip, idxArr) {
 					if (flip) {
 						idxArr[0] = sc.model.player.config.headIdx;
 					}
 					return old.apply(this, arguments);
-				}
-			}
+				};
+			},
 		});
 
 		function customNameToHeadIdx(headIdx) {
-			if(typeof headIdx == "string" && headIdx in customNameMapping) {
+			if (typeof headIdx == "string" && headIdx in customNameMapping) {
 				return customNameMapping[headIdx];
 			}
 			return headIdx;
@@ -93,36 +83,33 @@ export default class ExtendableHeads extends Plugin  {
 
 		sc.PartyMemberModel.inject({
 			getHeadIdx() {
-				return customNameToHeadIdx(this.parent())
-			}
-		})
-		
+				return customNameToHeadIdx(this.parent());
+			},
+		});
+
 		ig.ENTITY.Enemy.inject({
 			getHeadIdx() {
-				return customNameToHeadIdx(this.parent())
-			}
-		})
+				return customNameToHeadIdx(this.parent());
+			},
+		});
 
 		const img = new ig.Image("media/gui/severed-heads.png");
 		img.addLoadListener({
-			onLoadableComplete: function(loaded, image) {
+			onLoadableComplete: function (loaded, image) {
 				// replace image
 				if (loaded) {
 					if (!image.failed) {
 						image.width = newImage.width;
 						image.height = newImage.height;
-						
+
 						image.data = newImage;
 					}
-					
 				}
-			}
+			},
 		});
-
 	}
 	async loadImage(src) {
-		let imgData = new Image;
-		
+		let imgData = new Image();
 
 		await new Promise((resolve, reject) => {
 			imgData.onload = () => {
@@ -141,7 +128,7 @@ export default class ExtendableHeads extends Plugin  {
 	getTotalDim(imgs) {
 		let dim = {
 			x: 0,
-			y: 0
+			y: 0,
 		};
 		for (const img of imgs) {
 			dim.x += img.width;
@@ -151,8 +138,7 @@ export default class ExtendableHeads extends Plugin  {
 	}
 	async mergeImages(imgs) {
 		const canvas = document.createElement("canvas");
-		
-		
+
 		// preset canvas
 
 		// normalize the width
@@ -163,8 +149,8 @@ export default class ExtendableHeads extends Plugin  {
 		const xPos = [];
 		for (const img of imgs) {
 			xPos.push(width);
-			const remainder = (24 - (img.width%24));
-			if (remainder%24 === 0) {
+			const remainder = 24 - (img.width % 24);
+			if (remainder % 24 === 0) {
 				width += img.width;
 			} else {
 				// round up
@@ -179,14 +165,14 @@ export default class ExtendableHeads extends Plugin  {
 		// want to go by a factor of 24
 		// go one by one
 		for (const img of imgs) {
-			ctx.drawImage(img, xPos.shift(), 0);			
+			ctx.drawImage(img, xPos.shift(), 0);
 		}
 		return await this.loadImage(canvas.toDataURL("image/png"));
 	}
 
 	calcStartIndex(dim, extraImgs) {
 		// assume each head is 24x24
-		return (dim.x - (extraImgs.length * 24))/24;
+		return (dim.x - extraImgs.length * 24) / 24;
 	}
 
 	getHeadIdx() {
@@ -194,13 +180,14 @@ export default class ExtendableHeads extends Plugin  {
 			$.ajax({
 				dataType: "json",
 				url: "data/players/headIdx.json",
-				success: (data) => {
-					resolve(data)
+				success: data => {
+					resolve(data);
 				},
 				error: () => {
 					reject();
-				}			
-			})
+				},
+			});
 		});
 	}
 }
+
